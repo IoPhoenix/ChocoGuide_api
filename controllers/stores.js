56c75b615a10a6a2dd843becd68be06792169ffb
IoPhoenix <olga.fomin26@gmail.com>
2018-07-19 13:15:16 -0700

    Updated fetch params

--- controllers/stores.js
+++ controllers/stores.js
@@ -3,21 +3,21 @@
 const yelp = require('yelp-fusion');
 
 const fetchStores = (req, res) => {
-    const zip = req.body.location;
-    const apiKey = 'my_api_key';
-    const client = yelp.client(apiKey);
+  const zip = req.body.location;
+  const apiKey = 'my_api_key';
+  const client = yelp.client(apiKey);
 
-client.search({
-    term:'store',
-    location: zip,
-    categories: 'candy,chocolate',
-    limit: '10',
-    radius: '3200'
-  }).then(response => {
-    res.send(response.jsonBody);
-  }).catch(e => {
-    console.log(e);
-  });
+  client.search({
+      term: 'store',
+      location: zip,
+      categories: 'candy,chocolate,jpsweets',
+      limit: '10',
+      radius: '3200'
+    }).then(response => {
+      res.send(response.jsonBody);
+    }).catch(error => {
+      console.log('Could not fetch data from Yelp: ', error);
+    });
 }
 
 module.exports = {
