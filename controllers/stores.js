8a985bcf12a4cb95f779d07a4c5e8e11da49b4c8
IoPhoenix <olga.fomin26@gmail.com>
2018-07-18 23:39:08 -0700

    Update return of data to browser

--- controllers/stores.js
+++ controllers/stores.js
@@ -3,23 +3,21 @@
 const yelp = require('yelp-fusion');
 
 const fetchStores = (req, res) => {
-        const { parameters } = req.body;
-        const apiKey = 'my_api_key';
-        const client = yelp.client(apiKey);
+    const zip = req.body.location;
+    const apiKey = 'my_api_key';
+    const client = yelp.client(apiKey);
 
-    client.search({
-        term:'store',
-        location: parameters.location,
-        categories: 'candy,chocolate',
-        limit: '10',
-        radius: '3200'
-      }).then(response => {
-        const firstResult = response.jsonBody.businesses;
-        const prettyJson = JSON.stringify(firstResult, null, 4);
-        res.json(prettyJson);
-      }).catch(e => {
-        console.log(e);
-      });
+client.search({
+    term:'store',
+    location: zip,
+    categories: 'candy,chocolate',
+    limit: '10',
+    radius: '3200'
+  }).then(response => {
+    res.send(response.jsonBody);
+  }).catch(e => {
+    console.log(e);
+  });
 }
 
 module.exports = {
